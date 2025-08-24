482beaa119df87f7827f461724277f32b8adb30d
milktea <yuribling123@gmail.com>
2025-08-24 12:15:41 -0700

    prisma

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,44 +1,44 @@
-import { PrismaClient, Prisma } from "../src/generated/prisma";
+// import { PrismaClient, Prisma } from "../src/generated/prisma";
 
-const prisma = new PrismaClient();
+// const prisma = new PrismaClient();
 
-const userData: Prisma.UserCreateInput[] = [
-  {
-    name: "Alice",
-    email: "alice@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Join the Prisma Discord",
-          content: "https://pris.ly/discord",
-          published: true,
-        },
-        {
-          title: "Prisma on YouTube",
-          content: "https://pris.ly/youtube",
-        },
-      ],
-    },
-  },
-  {
-    name: "Bob",
-    email: "bob@prisma.io",
-    posts: {
-      create: [
-        {
-          title: "Follow Prisma on Twitter",
-          content: "https://www.twitter.com/prisma",
-          published: true,
-        },
-      ],
-    },
-  },
-];
+// const userData: Prisma.UserCreateInput[] = [
+//   {
+//     name: "Alice",
+//     email: "alice@prisma.io",
+//     posts: {
+//       create: [
+//         {
+//           title: "Join the Prisma Discord",
+//           content: "https://pris.ly/discord",
+//           published: true,
+//         },
+//         {
+//           title: "Prisma on YouTube",
+//           content: "https://pris.ly/youtube",
+//         },
+//       ],
+//     },
+//   },
+//   {
+//     name: "Bob",
+//     email: "bob@prisma.io",
+//     posts: {
+//       create: [
+//         {
+//           title: "Follow Prisma on Twitter",
+//           content: "https://www.twitter.com/prisma",
+//           published: true,
+//         },
+//       ],
+//     },
+//   },
+// ];
 
-export async function main() {
-  for (const u of userData) {
-    await prisma.user.create({ data: u });
-  }
-}
+// export async function main() {
+//   for (const u of userData) {
+//     await prisma.user.create({ data: u });
+//   }
+// }
 
-main();
+// main();
