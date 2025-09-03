def6b447a3bd4b5e3151e04993e96f3286c03f1f
milktea <yuribling123@gmail.com>
2025-09-03 12:56:43 -0700

    trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,15 @@
 "use client"
 import { useTRPC } from "@/trpc/client";
+import { getQueryClient } from "@/trpc/server";
 import Image from "next/image";
 
 
 
 const Home = () => {
-  const trpc = useTRPC();
-  trpc.hello .queryOptions({text:"hello"})
+  // const trpc = useTRPC();
+  // trpc.hello .queryOptions({text:"hello"})
+  const queryClient = getQueryClient
+  
   
   return (
     <div>
