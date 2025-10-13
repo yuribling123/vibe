1d25e33c414910f48e0b4c872945fbb82af4d344
milktea <yuribling123@gmail.com>
2025-10-13 13:51:40 -0400

    trpc-client

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -5,8 +5,12 @@ import { useTRPC } from "@/trpc/client";
 
    const Client = () => {
     const trpc = useTRPC();
-    // const {data} = useSuspenseQueries(trpc.createAI.queryOptions({text:""}) )
-    
-   }
+    // const {data} = useSuspenseQueries(trpc.createAI.queryOptions({text:"Antonio Prefetch"}) )
+    return (
+     <div>
+      {/* {JSON.stringify(data)} */}
+     </div>
+    )
+   }  
     
    export default Client;  
