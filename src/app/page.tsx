40175db6013894aa88dfb25f989ca4b50e2d2620
milktea <yuribling123@gmail.com>
2025-10-22 19:11:16 -0400

    trpc

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,19 @@
 "use client";
 
-const Page = () => {
+import { useTRPC } from "@/trpc/client";
+import { useQuery } from "@tanstack/react-query";
+
+const Page =  () => {
   // const queryClient = getQueryClient();
   // void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: 123 }));
+  const trpc = useTRPC();
+  const {data} = useQuery(trpc.createAI.queryOptions({ text: 123 }));
 
   return (  
     // <HydrationBoundary state={dehydrate(queryClient)}>
     //   <Client />  // Cannot find name 'Client'.
     // </HydrationBoundary>
-    <div>hello world</div>
+    <div>{JSON.stringify(data)}</div>
   );
 }
 
