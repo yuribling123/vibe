fa85c378eb10068473ed016dc7da94f1133bfd5b
milktea <yuribling123@gmail.com>
2025-10-22 22:40:18 -0400

    trpc-complete

--- src/app/client.tsx
+++ src/app/client.tsx
@@ -1,12 +1,15 @@
 "use client";
 import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 export const Client = () => {
   const trpc = useTRPC();
   const { data } = useSuspenseQuery(
-    trpc.createAI.queryOptions({ text: 123 })
+    trpc.createAI.queryOptions({ text: 123  })
   );
+
+  useEffect(() => {}, []); 
 
   return (
     <div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,26 @@
 
 import { useTRPC } from "@/trpc/client";
-import { caller } from "@/trpc/server";
-import { useQuery } from "@tanstack/react-query";
+import { caller, getQueryClient, trpc } from "@/trpc/server";
+import { dehydrate, HydrationBoundary} from "@tanstack/react-query";
 import { text } from "stream/consumers";
+import { Client } from "./client";
+import { Suspense } from "react";
+// the moment sever component load, client will prefetch the data 
 
 const Page = async () => {
-  // const queryClient = getQueryClient();
-  // void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: 123 }));
+  const queryClient = getQueryClient();
+  void queryClient.prefetchQuery(trpc.createAI.queryOptions({ text: 123 }));
   // const trpc = useTRPC();
   // const {data} = useQuery(trpc.createAI.queryOptions({ text: 123 })); this is the client option
-  const data = await caller.createAI({text:123}); // this is the server option
+  // const data = await caller.createAI({text:123}); // this is the server option
 
   return (  
-    // <HydrationBoundary state={dehydrate(queryClient)}>
-    //   <Client />  // Cannot find name 'Client'.
-    // </HydrationBoundary>
-    <div>{JSON.stringify(data)}</div>
+    <HydrationBoundary state={dehydrate(queryClient)}>
+      <Suspense fallback={<p>Loading...</p>}>
+      <Client />  
+      </Suspense>
+     </HydrationBoundary>
+  
   );
 }
 
