f2bb655d6fa82f8ae11909854bc80db10b5378e7
milktea <yuribling123@gmail.com>
2025-11-17 18:41:53 -0500

    front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
+"use client"
+import { Button } from "@/components/ui/button";
+import { useTRPC } from "@/trpc/client";
+import { useMutation } from "@tanstack/react-query";
+import { use } from "react";
 
-const Page = async () => {
+const Page = () => {
 
+  const trpc = useTRPC();
+  const invoke = useMutation(trpc.invoke.mutationOptions({}));
+
+
+ 
   return (  
-   <div>
-    test
+   <div className="p-4 max-w-7xl mx-auto" >
+
+     <Button onClick={()=> invoke.mutate({text:"John "} )}>
+      Invoke Background Job
+     </Button>
+
    </div>
   
   );
