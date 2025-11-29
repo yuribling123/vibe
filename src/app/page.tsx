94486c904b2f208f09abf360e06948590441add1
milktea <yuribling123@gmail.com>
2025-11-28 23:39:29 -0500

    front page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 "use client"
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
 import { useTRPC } from "@/trpc/client";
 import { useMutation } from "@tanstack/react-query";
-import { use } from "react";
+import { use, useState } from "react";
 import { toast } from "sonner";
 
 const Page = () => {
 
   //   create open ai key
+  const [value,setValue] = useState("")
+    
 
   const trpc = useTRPC();
   const invoke = useMutation(trpc.invoke.mutationOptions({
@@ -20,7 +23,7 @@ const Page = () => {
  
   return (  
    <div className="p-4 max-w-7xl mx-auto" >
-
+    <Input></Input>
      <Button disabled={invoke.isPending}  onClick={()=> invoke.mutate({text:"John "} )}>
       Invoke Background Job
      </Button>
