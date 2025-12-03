ebcabf2a62d7d815590b35662b9240a07a4c0c58
milktea <yuribling123@gmail.com>
2025-12-02 22:45:49 -0500

    summarizer

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -5,24 +5,23 @@ import {Agent,openai,createAgent} from "@inngest/agent-kit";
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
-  async ({ event, step }) => {
+  async ({ event }) => {
 
     const summarizer = createAgent({
       name: "summarizer",
       system:"you are an expert summari zer. You summarize in two words",
       model: openai({model:"gpt-4o"}),
     });
+    
     const {output} =  await summarizer.run(
-      `Summarize the following text in two words : ${event.data.value}`
+      `Summarize the following text : ${event.data.value}`
     )
   
     console.log(output)
 
 
-
-
     // download step
-    await step.sleep("wait-a-moment", "5s");
+    // await step.sleep("wait-a-moment", "5s");
  
     //  
     return { output};
