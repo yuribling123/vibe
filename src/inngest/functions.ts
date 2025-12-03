236b6adb4f7551b3f1c031fea12568d096203442
milktea <yuribling123@gmail.com>
2025-12-02 22:27:07 -0500

    summarizer

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,3 +1,4 @@
+import { success } from "zod";
 import { inngest } from "./client";
 import {Agent,openai,createAgent} from "@inngest/agent-kit";
 
@@ -8,9 +9,14 @@ export const helloWorld = inngest.createFunction(
 
     const summarizer = createAgent({
       name: "summarizer",
-      system:"you are an expert summarizer. You summarize in two words",
+      system:"you are an expert summari zer. You summarize in two words",
       model: openai({model:"gpt-4o"}),
     });
+    const {output} =  await summarizer.run(
+      `Summarize the following text in two words : ${event.data.value}`
+    )
+  
+    console.log(output)
 
 
 
@@ -19,6 +25,6 @@ export const helloWorld = inngest.createFunction(
     await step.sleep("wait-a-moment", "5s");
  
     //  
-    return { message: `Hello ${event.data.value}!` };
+    return { output};
   },
 ); 
