226e05d2bb17920222639f593e5a7c474ae5b2e3
milktea <yuribling123@gmail.com>
2026-02-21 15:23:37 -0500

    ai agent function

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -1,35 +1,44 @@
 import { success } from "zod";
 import { inngest } from "./client";
-import { Agent, openai, createAgent } from "@inngest/agent-kit";
+import { Agent, openai, createAgent, createTool } from "@inngest/agent-kit";
 import { Sandbox } from "@e2b/code-interpreter";
 import { getSandbox } from "./utils"
+import { de } from "date-fns/locale";
+import { z } from "zod";
 
 export const helloWorld = inngest.createFunction(
   { id: "hello-world" },
   { event: "test/hello.world" },
   async ({ event, step }) => {
 
+    // Get sandbox id from e2b
     const sandboxId = await step.run("get-sandbox-id", async () => {
       const sandbox = await Sandbox.create("vibe-nextjs-test-2");
       return sandbox.sandboxId;
     });
 
-    const summarizer = createAgent({
-      name: "summarizer",
+    // Create an agent with a system prompt and a model
+    const codeAgent = createAgent({
+      name: "code-agent",
       system: "you are an expert Next JS developer. You write readable maintable code, You write simple Next JS and React Snippets.",
       model: openai({ model: "gpt-4o" }),
+      tools:[createTool({name:"Terminal",description:"Use the terminal to run commands",parameters:z.object({command:z.string()}),handler:async(input)=>{return`Executed: ${input.command}`}})]
     });
 
-    const { output } = await summarizer.run(
+
+    // Run the code agent with the input prompt and get the output
+    const { output } = await codeAgent.run(
       `Write the Following Snippet : ${event.data.value}`
     )
-    // get url of running sandbox
+
+
+    // Get the sandbox URL 
     const sandboxUrl = await step.run("get-sandbox-url", async () => {
-    const sandbox = await getSandbox(sandboxId);
+      const sandbox = await getSandbox(sandboxId);
 
-    const host = sandbox.getHost(3000);
+      const host = sandbox.getHost(3000);
 
-    return `https://${host}`;
+      return `https://${host}`;
     });
 
 
