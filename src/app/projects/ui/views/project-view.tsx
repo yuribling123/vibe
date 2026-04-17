9af10d94bfe6e0ab36a3e3d2a1fbf2bcb597d4af
milktea <yuribling123@gmail.com>
2026-04-17 00:44:23 -0400

    project/ui

--- src/app/projects/ui/views/project-view.tsx
+++ src/app/projects/ui/views/project-view.tsx
@@ -1,24 +1,30 @@
-"use client"
+"use client";
 
-import { trpc } from "@/trpc/server";
+import { useTRPC } from "@/trpc/client";
 import { useSuspenseQuery } from "@tanstack/react-query";
 
-interface Props{
-    projectId: string
+interface Props {
+  projectId: string;
 }
 
+const ProjectView = ({ projectId }: Props) => {
+  const trpc = useTRPC();
 
-const ProjectView = ({projectId}: Props) => {
-    // fetch project and messages and diplay when ready using suspense query
-    const {data:project} = useSuspenseQuery(trpc.projects.getOne.queryOptions({id:projectId})); 
-    const {data:messages} = useSuspenseQuery(trpc.messages.getMany.queryOptions({projectId:projectId}));
-    return ( 
-        <div>
-            <h1>Project View</h1>
-            {JSON.stringify(project)}
-            {JSON.stringify(messages,null,2)}
-        </div>
-     );
-}
- 
+  const { data: project } = useSuspenseQuery(
+    trpc.projects.getOne.queryOptions({ id: projectId })
+  );
+
+  const { data: messages } = useSuspenseQuery(
+    trpc.messages.getMany.queryOptions({ projectId })
+  );
+
+  return (
+    <div>
+      <h1>Project View</h1>
+      {JSON.stringify(project)}
+      {JSON.stringify(messages, null, 2)}
+    </div>
+  );
+};
+
 export default ProjectView;
