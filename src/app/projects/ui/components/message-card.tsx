473e709ae2f2bcb7cd6308c284599e4edef42cc8
milktea <yuribling123@gmail.com>
2026-05-04 20:42:09 -0400

    project page/ui

--- src/app/projects/ui/components/message-card.tsx
+++ src/app/projects/ui/components/message-card.tsx
@@ -1,4 +1,6 @@
 import { Fragment, MessageRole, MessageType } from "@/generated/prisma";
+import { cn } from "@/lib/utils";
+import {format} from "date-fns";
 
 interface MessageCardProps {
     content: string;
@@ -8,6 +10,33 @@ interface MessageCardProps {
     isAcitiveFragment: boolean;
     onFragmentClick: (fragment: Fragment) => void;
     type: MessageType;
+}
+
+interface AssistantMessageProps {
+    content: string;
+    fragement: Fragment | null;
+    createdAt: Date;
+    isAcitiveFragment: boolean;
+    onFragmentClick: (fragment: Fragment) => void;
+    type: MessageType;
+}
+
+const AssistantMessage = ({
+    content,
+    fragement,
+    createdAt,
+    isAcitiveFragment,
+    onFragmentClick,
+    type
+}: AssistantMessageProps) => {
+    return (
+        <div className={cn("flex items-center",type==="ERROR"&&"text-red-700" )}>
+           <div className="flex items-center gap-2 pl-2 mb-2">
+            <span className="text-sm font-medium">Vibe</span>
+            <span className="text-sm text-muted-foreground opacity-0 transition-opacity">{format(createdAt, "hh:mm:ss a")}</span>
+           </div>
+        </div>
+    )
 }
 
 const MessageCard = (
@@ -21,11 +50,18 @@ const MessageCard = (
 ) => {
     if (role === "ASSISTANT") {
         return (
-            <p>Assistant</p>
+            <AssistantMessage
+                content={content}
+                fragement={fragment}
+                createdAt={createdAt}
+                isAcitiveFragment={isAcitiveFragment}
+                onFragmentClick={onFragmentClick}
+                type={type}
+            />
         )
     }
     return (
-        <p>User</p>
+        <p>{type}</p>
     );
 }
 
