c7966b80e4884fc968a83e4032ab482432f4d929
milktea <yuribling123@gmail.com>
2026-05-20 17:21:48 -0400

    fragment-preview/hint

--- src/app/projects/ui/components/fragment-web.tsx
+++ src/app/projects/ui/components/fragment-web.tsx
@@ -3,6 +3,7 @@ import { Fragment } from "@/generated/prisma";
 import { ExternalLinkIcon, RefreshCcwIcon } from "lucide-react";
 import { useState } from "react";
 import { set } from "zod";
+import Hint from "../hint";
 
 interface Props{
     data:Fragment
@@ -26,21 +27,25 @@ const FragmentWeb = ({data}:Props) => {
         <div className="flex flex-col w-full h-full">
 
             <div className="p-2 border-b bg-sidebar flex item-center gap-x-2">
+                <Hint text="refresh" side="bottom" align="start">
                 <Button size="sm" variant="outline" onClick={onRefresh} >
                     <RefreshCcwIcon />
                 </Button>
+                </Hint>
 
-
+                <Hint text="click to copy" side="bottom" align="start">
                 <Button size="sm" variant="outline" onClick={handleCopy} disabled={copied || !data.sandboxUrl} className="flex-1 justify-start text-start front-normal">
                     <span className="truncate">
                         {data.sandboxUrl}
                     </span>
                 </Button>
+                </Hint>
 
-
+                <Hint text="Open in new tab" side="bottom" align="start">
                 <Button size="sm" disabled={!data.sandboxUrl} variant="outline" onClick={()=>{if(!data.sandboxUrl) return; window.open(data.sandboxUrl, "_blank");}} >
                     <ExternalLinkIcon/>
                 </Button>
+                </Hint>
 
             </div>
 
--- src/app/projects/ui/hint.tsx
+++ src/app/projects/ui/hint.tsx
@@ -9,3 +9,22 @@ interface Props{
     side?: "top" | "bottom" | "left" | "right";
     align?: "start" | "center" | "end";
 }
+
+const Hint = ({text, children, side, align}:Props) => {
+    return ( 
+        <TooltipProvider>
+            <Tooltip>
+                <TooltipTrigger asChild>
+                    {children}
+                </TooltipTrigger>
+                <TooltipContent side={side} align={align}>
+                    {text}
+                </TooltipContent>
+            </Tooltip>
+        </TooltipProvider>
+
+
+     );
+}
+ 
+export default Hint;
