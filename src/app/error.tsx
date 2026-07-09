13973df232bfe8a971bd162d49858f86919e41fd
milktea <yuribling123@gmail.com>
2026-07-09 22:58:37 +0800

    sanbox+error handle

---
+++ src/app/error.tsx
@@ -0,0 +1,13 @@
+"use client"
+
+const ErrorPage = () => {
+    return (
+
+        <div>
+            Global error
+        </div>
+
+    ) 
+}
+
+export default ErrorPage;
--- src/app/projects/[projectId]/page.tsx
+++ src/app/projects/[projectId]/page.tsx
@@ -1,6 +1,11 @@
+// 1. HydrationBoundary: Passes the data fetched on the server to the client, so it doesn't have to fetch it again.'
+// 2. ErrorBoundary: If something inside crashes, show a fallback instead of breaking the whole page.
+// 3. Suspense: While waiting for data, show a loading UI.
 import { getQueryClient,trpc } from "@/trpc/server";
 import { dehydrate, HydrationBoundary } from "@tanstack/react-query";
 import ProjectView from "../ui/views/project-view";
+import {ErrorBoundary} from "react-error-boundary";
+import { Suspense } from "react";
 
 // the page will receive a param, which after wait becomes the projectId
 interface Props {
@@ -19,7 +24,11 @@ const Page = async ({ params }: Props) => {
     return (
         // use the hydration boundary to pass the prefetched data to the client
         <HydrationBoundary state={dehydrate(queryClient)}>
+            <ErrorBoundary fallback={<p>Error!</p>}>
+            <Suspense fallback={<p>Loading Projects</p>}>
             <ProjectView projectId={projectId} />
+            </Suspense>
+            </ErrorBoundary>
         </HydrationBoundary>
     );
 }
